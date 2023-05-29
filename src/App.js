98d2ef637becff242979561b81057462511c1ca9
seifeddinesaad <saadseif1925@outlook.com>
2023-05-29 19:18:46 +0100

    section3 implmentation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/Navbar/NavBar';
 import Hero from './components/Hero/Hero';
 import SectionOne from './components/Section-1/SectionOne';
 import SectionTwo from './components/Section-2/SectionTwo';
+import SectionThree from './components/Section-3/SectionThree';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <SectionOne />
       <SectionTwo />
+      <SectionThree />
     </>
   );
 }
---
+++ src/components/Section-3/SectionThree.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const SectionThree = () => {
+  return (
+   <div className="section3__container">
+    <div className="section3__left">
+      <h1>About Us</h1>
+      <p style={{marginTop:'-1rem'}}>Do You Have Any Question?</p>
+      <button className='hero__btn'>Contact Us</button>
+      {/* <div className="section3__icons">
+        <p>facebook</p>
+        <p>instagram</p>
+      </div> */}
+    </div>
+    <div className="section3__right">
+        <div className="section3__card">
+          <p>B2B Sales Outsourcing Services.
+            Saravá Sales makes Businesses and Startups grow in terms</p>
+        </div>
+        <div className="section3__card">
+          <p>B2B Sales Outsourcing Services.
+            Saravá Sales makes Businesses and Startups grow in terms</p>
+        </div>
+        <div className="section3__card">
+          <p>B2B Sales Outsourcing Services.
+            Saravá Sales makes Businesses and Startups grow in terms</p>
+        </div>
+        <div className="section3__card">
+          <p>B2B Sales Outsourcing Services.
+            Saravá Sales makes Businesses and Startups grow in terms</p>
+        </div>
+    </div>
+   </div>
+  )
+}
+
+export default SectionThree
