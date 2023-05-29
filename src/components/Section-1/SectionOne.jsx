80ce368e214e7e2abcf6b566d9e4d4e5cd627052
seifeddinesaad01 <saadseif1925@outlook.com>
2023-05-29 15:50:20 +0100

    adding cards

--- src/components/Section-1/SectionOne.jsx
+++ src/components/Section-1/SectionOne.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import card1Img from "../../assets/card1.png";
-import card2Img from "../../assets/card2.png"
+import card2Img from "../../assets/card2.png";
+import card3Img from "../../assets/card3.png"
 
 const SectionOne = () => {
   return (
@@ -20,7 +21,7 @@ const SectionOne = () => {
             <p style={{marginLeft:'-2.5rem', marginTop:'2rem', fontWeight:'bold'}}>More Clients</p>
         </div>
         <div className="section1__card">
-            <img className="card__img" src={card1Img} alt="card" />
+            <img className="card__img" src={card3Img} alt="card" />
             <h2 style={{color: '#228EEB', marginTop:'-1.1rem'}}>Increase</h2>
             <p style={{marginLeft:'-2.5rem', marginTop:'2rem', fontWeight:'bold'}}>Your Sales</p>
         </div>
