ca41523957e8302edcdcfb7547b0b74c8f30df81
seifeddinesaad01 <saadseif1925@outlook.com>
2023-05-30 10:31:29 +0100

    form implementation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
 const Form = () => {
-  return <div className="form__container">
-    <div className="form__left">
-
+  return (
+    <div className="form__container">
+      <div className="form__left">
+        <h2 style={{color: '#fff', fontSize: '4rem'}}>Let's Talk</h2>
+        <p style={{color: '#fff', fontSize: '1.4rem'}}>Tell us abit about your needs and let’s explore some solution ideas.</p>
+        <button className="hero__btn">Book a videocall</button>
+      </div>
+      <form className="form">
+        <label>Name</label>
+        <input type="text" style={{border: 'none', borderBottom: '1px solid black'}} />
+        <label>Email</label>
+        <input type="text" style={{border: 'none', borderBottom: '1px solid black'}} />
+        <label>Number</label>
+        <input type="text" style={{border: 'none', borderBottom: '1px solid black'}} />
+        <label>Company</label>
+        <input type="text" style={{border: 'none', borderBottom: '1px solid black'}} />
+        <button className="hero__btn">Send</button>
+      </form>
     </div>
-    <form></form>
-  </div>;
+  );
 };
 
 export default Form;
