244c39f5baa451597336afda5a3e84cfc26d7d51
seifeddinesaad01 <saadseif1925@outlook.com>
2023-05-30 11:09:47 +0100

    some changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
  <div className="hero__container">
     <div className="hero__top">
-      <h2 style={{fontSize:'4rem', lineHeight:'1px'}} className="hero__title">Boost yourgrowth & revenue</h2>
+      <h2 style={{fontSize:'3.5rem', lineHeight:'1px'}} className="hero__title">Boost your growth & revenue</h2>
       <p style={{fontSize: '1.4rem'}}>Borem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <button className='hero__btn'>Contact Us</button>
     </div>
--- src/components/Section-6/SectionSix.jsx
+++ src/components/Section-6/SectionSix.jsx
@@ -4,34 +4,35 @@ const SectionSix = () => {
   return (
     <div className="section6__container">
       <div className="section6__left">
-        <h2>Blog</h2>
+        <h2 style={{fontSize: '4rem', marginTop:'-14rem'}}>Blog</h2>
       </div>
       <div className="section6__cards">
         <div className="section6__card">
-          <h3>Lorem</h3>
+          <h2 style={{paddingTop: '10rem'}} >Lorem</h2>
+          <p style={{fontSize: '1.1rem'}}>
+            Rorem ipsum dolor <br /> sit amet, consectetur <br /> adipiscing
+            elit. Etiam eu <br /> turpis molestie, dictum <br /> est a, mattis
+            tellus.
+          </p>
+        </div>
+        <div className="section6__card1">
+          <h2 style={{paddingTop: '10rem'}}>Lorem</h2>
           <p>
             Rorem ipsum dolor <br /> sit amet, consectetur <br /> adipiscing
             elit. Etiam eu <br /> turpis molestie, dictum <br /> est a, mattis
             tellus.
           </p>
         </div>
-        <div className="section6__card">
-          <h3>Lorem</h3>
+        <div className="section6__card2">
+          <h2 style={{paddingTop: '10rem'}}>Lorem</h2>
           <p>
             Rorem ipsum dolor <br /> sit amet, consectetur <br /> adipiscing
             elit. Etiam eu <br /> turpis molestie, dictum <br /> est a, mattis
             tellus.
           </p>
         </div>
-        <div className="section6__card">
-          <h3>Lorem</h3>
-          <p>
-            Rorem ipsum dolor <br /> sit amet, consectetur <br /> adipiscing
-            elit. Etiam eu <br /> turpis molestie, dictum <br /> est a, mattis
-            tellus.
-          </p>
         </div>
-      </div>
+      <button className="hero__btn" style={{alignSelf: 'flex-end', justifySelf: 'flex-end'}}>See All</button>
     </div>
   );
 };
